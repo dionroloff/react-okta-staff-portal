85a8b06bbb48dbe93487403165c7e72a2657f205
Dion Roloff <dion.roloff@gmail.com>
2019-07-27 11:24:19 -0500

    established navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from "./components/Layout/NavBar";
+import Home from './components/Pages/Home';
+import Staff from './components/Pages/Staff';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route path="/" exact={ true } component={ Home } />
+          <Route path="/staff" exact={ true } component={ Staff } />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
